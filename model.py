de1af78ca916b985c0f7c74b06fe5e4cb36fc254
luongdangvp2000 <luongdangvp2000@gmail.com>
2022-03-06 15:58:02 +0000

    fix bug in model.py

--- model.py
+++ model.py
@@ -1,28 +1,30 @@
 import torch
 import torch.nn as nn
-import torchvision.transform.functional as TF
+import torchvision.transforms.functional as TF
 
 class DoubleConv(nn.Module):
-    def __init__ (self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels):
         super(DoubleConv, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3,2,1, bias=False),
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels, out_channels, 3,2,1, bias=False),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
         )
-    
+
     def forward(self, x):
         return self.conv(x)
 
-class UNET (nn.Module):
-    def __init__(self, in_channels=3, out_channels=1, feature=[64,128,256,512]):
+class UNET(nn.Module):
+    def __init__(
+            self, in_channels=3, out_channels=1, features=[64, 128, 256, 512],
+    ):
         super(UNET, self).__init__()
         self.ups = nn.ModuleList()
         self.downs = nn.ModuleList()
-        self.pool = nn.MaxPool2d (kernel_size=2, stride=2)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         # Down part of UNET
         for feature in features:
@@ -48,17 +50,16 @@ class UNET (nn.Module):
             x = down(x)
             skip_connections.append(x)
             x = self.pool(x)
-        
+
         x = self.bottleneck(x)
         skip_connections = skip_connections[::-1]
 
         for idx in range(0, len(self.ups), 2):
             x = self.ups[idx](x)
             skip_connection = skip_connections[idx//2]
-            
+
             if x.shape != skip_connection.shape:
                 x = TF.resize(x, size=skip_connection.shape[2:])
-            
 
             concat_skip = torch.cat((skip_connection, x), dim=1)
             x = self.ups[idx+1](concat_skip)
@@ -66,7 +67,7 @@ class UNET (nn.Module):
         return self.final_conv(x)
 
 def test():
-    x = torch.randn((3,1,160,160))
+    x = torch.randn((3, 1, 161, 161))
     model = UNET(in_channels=1, out_channels=1)
     preds = model(x)
     print(preds.shape)
