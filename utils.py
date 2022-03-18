b24324669614a9a5c4a9bf4d7b8a437a33433b84
luongdangvp2000 <luongdangvp2000@gmail.com>
2022-03-18 02:55:42 +0000

    upload iou score

--- utils.py
+++ utils.py
@@ -77,21 +77,26 @@ def check_accuracy_dice_score(loader, model, device="cuda"):
     print(f"Dice score: {dice_score/len(loader)}")
     model.train()
 
-# def check_accuracy_iou_score(loader, model, device = "cuda"):
-#     num_correct = 0
-#     num_pixels = 0
-#     iou_score = 0
-#     model.eval()
+def check_accuracy_iou_score(loader, model, device = "cuda"):
+    num_correct = 0
+    num_pixels = 0
+    iou_score = 0
+    model.eval()
 
-#     with torch.no_grad():
-#          for x, y in loader:
-#             x = x.to(device)
-#             y = y.to(device).unsqueeze(1)
-#             preds = torch.sigmoid(model(x))
-#             preds = (preds > 0.5).float()
-#             num_correct += (preds == y).sum()
-#             num_pixels += torch.numel(preds)
-#             iou_score += ((preds*y).sum())/
+    with torch.no_grad():
+         for x, y in loader:
+            x = x.to(device)
+            y = y.to(device).unsqueeze(1)
+            preds = torch.sigmoid(model(x))
+            preds = (preds > 0.5).float()
+            num_correct += (preds == y).sum()
+            num_pixels += torch.numel(preds)
+            iou_score += ((preds*y).sum())/((preds + y).sum() + 1e-8 - (preds*y).sum())
+    print(
+        f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100:.2f}"
+    )
+    print(f"IoU score: {iou_score/len(loader)}")
+    model.train()       
 
 def save_predictions_as_imgs(
     loader, model, folder="saved_images/", device="cuda"
