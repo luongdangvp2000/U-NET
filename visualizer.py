46083e76cbcf1bbaaaa06b06e5e180748ed5882e
luongdangvp2000 <luongdangvp2000@gmail.com>
2022-03-25 05:59:13 +0000

    test visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 from torch.utils.tensorboard import SummaryWriter
 
 from utils import (
@@ -11,6 +14,20 @@ from utils import (
     check_accuracy_iou_score,
     save_predictions_as_imgs,
 )
+
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 16
+NUM_EPOCHS = 3
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 160  # 1280 originally
+IMAGE_WIDTH = 240  # 1918 originally
+PIN_MEMORY = True
+LOAD_MODEL = False
+TRAIN_IMG_DIR = "/content/drive/MyDrive/U-NET/Dataset/train_images"
+TRAIN_MASK_DIR = "/content/drive/MyDrive/U-NET/Dataset/train_masks"
+VAL_IMG_DIR = "/content/drive/MyDrive/U-NET/Dataset/val_images"
+VAL_MASK_DIR = "/content/drive/MyDrive/U-NET/Dataset/val_masks"
 
 # def matplotlib_imshow(img, one_channel=False):
 #     if one_channel:
@@ -36,3 +53,52 @@ def plot_img_and_mask(img, mask):
         ax[1].imshow(mask)
     plt.xticks([]), plt.yticks([])
     plt.show()
+
+def test():
+    train_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Rotate(limit=35, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.1),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    val_transforms = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+    train_loader, val_loader = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
+        VAL_IMG_DIR,
+        VAL_MASK_DIR,
+        BATCH_SIZE,
+        train_transform,
+        val_transforms,
+        NUM_WORKERS,
+        PIN_MEMORY,
+    )
+
+    examples = iter(train_loader)
+    examples_data, examples_targets = examples.next()
+
+    for i in range(6):
+        plt.subplots(2,3,i+1)
+        plt.imshow(examples_data[i][0])
+
+if __name__ == "__main__":
+    test()
