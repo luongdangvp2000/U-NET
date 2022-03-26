e478f2f4e8b32f84e7a1dbe1deba14bb54474d83
luongdangvp2000 <luongdangvp2000@gmail.com>
2022-03-26 17:26:56 +0000

    test tensorboard

--- train.py
+++ train.py
@@ -115,8 +115,9 @@ def main():
 
     # Create a grid from the images and show them
     img_grid = torchvision.utils.make_grid(images)
-    # matplotlib_imshow(img_grid, one_channel=True)
-    writer.add_image('')
+    matplotlib_imshow(img_grid, one_channel=True)
+    writer.add_image('Four images', img_grid)
+    writer.flush()
 
 
     if LOAD_MODEL:
