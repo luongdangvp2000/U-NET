4178b32098b0f929158f832f9e0fdb6b8cce0063
luongdangvp2000 <luongdangvp2000@gmail.com>
2022-03-28 17:21:37 +0000

    fix sths

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -2,12 +2,12 @@ wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=180
 
 wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=1
 unzip gtFine_trainvaltest.zip
-mv gtFine_trainvaltest/* data/masks/
-rm -d gtFine_trainvaltest
+mv gtFine/* data/masks/
+rm -d gtFine
 rm gtFine_trainvaltest.zip
 
 wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=3
 unzip leftImg8bit_trainvaltest.zip
-mv leftImg8bit_trainvaltest/* data/images/
-rm -d leftImg8bit_trainvaltest
+mv leftImg8bit/* data/images/
+rm -d leftImg8bit
 rm leftImg8bit_trainvaltest.zip
